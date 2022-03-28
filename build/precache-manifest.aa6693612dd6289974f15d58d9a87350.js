9ad9d91720b53e38402ab1f5d88f87d41c336c4f
siramkalyan <siramkalyan@gmail.com>
2022-03-28 10:42:49 +0530

    build changes

---
+++ build/precache-manifest.aa6693612dd6289974f15d58d9a87350.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53348b65e6729510bac6780860efc7b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9283f6e304e872e6df7b",
+    "url": "/static/css/main.ea16a490.chunk.css"
+  },
+  {
+    "revision": "4e03c01373d87f16fa18",
+    "url": "/static/js/2.1816a793.chunk.js"
+  },
+  {
+    "revision": "dfeb6efdf769d3c0a9f57a97dc95a863",
+    "url": "/static/js/2.1816a793.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1903b8b65989977ea90",
+    "url": "/static/js/3.f9b96cf1.chunk.js"
+  },
+  {
+    "revision": "9283f6e304e872e6df7b",
+    "url": "/static/js/main.369434de.chunk.js"
+  },
+  {
+    "revision": "cf53557f2b2dda0dd2da",
+    "url": "/static/js/runtime-main.80f250bc.js"
+  }
+]);
